37b4339bc74447cc7bf0ae5f1cc29be1a31d6c82
Nikki <aimeeemm@hotmail.com>
2015-01-30 14:30:58 -0800

    search results change

--- app/models/expert.rb
+++ app/models/expert.rb
@@ -2,6 +2,7 @@ class Expert < ActiveRecord::Base
 	has_many :reviews
 
 	validates :first_name, :last_name, presence: true
+	validates :password,length:{ minimum: 6,maximum: 10 }
 	# Allows skills to be stored as an array
 	serialize :skills
 	
@@ -13,13 +14,9 @@ class Expert < ActiveRecord::Base
 		first_name.capitalize << " " << last_name[0].capitalize << "."
 	end
 	
-	#can be replaced by validation?
-	def has_full_name?
-		!first_name.nil? && !last_name.nil?
-	end
-
-	def update_rating
+		def update_rating
 		# Why does this work without self but without it doesn't
+		#that sentance makes no sense-N
 		return self.rating = nil if reviews.length < 1
 		self.rating = reviews.inject(0) { |sum, review| sum + review.rating }.to_f / reviews.length 
 		save
