50794fbf36a96412c0e94b7388a23f978ce19042
spiside <spirosideris@gmail.com>
2015-01-29 16:32:25 -0800

    Added update rating functionality

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,12 @@
 class Review < ActiveRecord::Base
-	validates :rating, inclusion: { in: [1,2] }
+	validates :rating, inclusion: { in: [0,1] }
 	
 	belongs_to :expert
+	
+	after_create :update_experts_rating
+
+	private
+		def update_experts_rating
+			expert.update_rating
+		end
 end
