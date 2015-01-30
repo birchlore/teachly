10409fa8d8f64bd490ba73daf0427192d3fe760b
birchlore <jacksondcunningham@gmail.com>
2015-01-30 22:41:27 +0000

    fetch expert from database

--- app/actions.rb
+++ app/actions.rb
@@ -12,7 +12,7 @@ get '/dashboard' do
 end
 
 get '/expert/:id' do
-  @expert = Expert.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: rand(20..40).floor, skills: ["quickbooks", "photoshop", "google analytics", "excel"], bio: Faker::Lorem.paragraph(sentence_count=8), email: Faker::Internet.email, password: Faker::Internet.password)
+  @expert = Expert.find(params[:id])
   erb :public_profile
 end
 
