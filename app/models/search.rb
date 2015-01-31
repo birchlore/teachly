1078c9960d43da5f4fec1c4dc49a4f2fa4414625
spiside <spirosideris@gmail.com>
2015-01-30 17:41:23 -0800

    Changed the actions.rb file to add in searching functionality

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,9 +1,10 @@
 class Search < ActiveRecord::Base
 extend ActiveModel::Naming
 
+	serialize :terms
   validate :error_msg
 
   def error_msg
-    @errors.add(:terms, "Please enter a search term") if terms.nil?
+    @errors.add(:terms, "Please enter a search term") if terms.empty?
   end
 end
